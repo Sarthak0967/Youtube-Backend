63815dccc25158b1236c764c453446d89bb9acff
Sarthak0967 <sarthakdoc09@gmail.com>
2025-03-27 14:31:30 +0530

    Till now we have done with complete industrygrade  setup for the project

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null,"./public/temp")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.filename)
+    }
+  })
+  
+export const upload = multer({ 
+    storage,
+})
